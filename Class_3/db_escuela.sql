df2370b3ecee0227b5ff39ed5d3247eedcb9744b
C-Sm1th <smithluna422@gmail.com>
2026-02-06 18:49:43 -0600

    cambio de nombre carpeta clase_4

--- Class_3/db_escuela.sql
+++ Class_3/db_escuela.sql
@@ -1,29 +1,47 @@
-create database dbEscuela;
+create database dbEstudiantes;
 
-use dbEscuela;
+use dbEstudiantes;
 
-create table Alumnos(
+create table tbl_estudiante(
 id int primary key auto_increment,
-nombre varchar(50),
-apellido varchar(50),
-edad int,
-grado varchar(100),
-correo varchar(100)
+nombre varchar(200),
+apellidos varchar(200),
+direccion varchar(200),
+telefono int,
+dui int,
+fecha_registro date
 );
 
-create table Profesores(
-id int primary key auto_increment,
-nombre varchar(50),
-apellido varchar(50),
-edad int,
-especialidad varchar(50),
-correo varchar(100),
-telefono int
+SELECT * FROM tbl_estudiante;
+-- SELECT nombre, apellidos FROM tbl_estudiante;
+
+INSERT INTO tbl_estudiante 
+( id, nombre, apellidos, direccion, telefono, dui, fecha_registro) VALUES(
+NULL,
+'Christian Smith',
+'Cruz Luna',
+'Col. Nueva Soria',
+79742410,
+45092093,
+'2026-02-05'
 );
 
-create table Aulas(
-id int primary key auto_increment,
-nombre varchar(20),
-capacidad int,
-edificio varchar(50)
+INSERT INTO tbl_estudiante 
+( id, nombre, apellidos, direccion, telefono, dui, fecha_registro) VALUES(
+NULL,
+'Adrian Jose',
+'Ayala Coreas',
+'Col. Palacios',
+78295647,
+738745637,
+'2026-02-05'
 );
+
+SELECT nombre as name, telefono as phone, fecha_registro as date from tbl_estudiante;
+
+SELECT * FROM tbl_estudiante where fecha_registro='2026-02-05';
+SELECT * FROM tbl_estudiante where id='2';
+
+-- DELETE FROM tbl_estudiante where id=2;
+
+UPDATE tbl_estudiante SET nombre='Manuel Henriquez' where id=5;
